809959380aa6ec8c9138840bf5269b65c1eafc00
acethekawaii <acegabriel0809@gmail.com>
2026-06-10 23:05:38 +0800

    prepare for vercel prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://assetwise-api.acethekawaii.work/api/v1',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
+  production: false,
   apiBaseUrl: 'http://localhost:8000/api/v1',
 };
